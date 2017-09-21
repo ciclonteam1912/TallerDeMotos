893185a3ad1f5dce372f91d5a1d140dff74d3b96
Juan Moris <moris_juan@hotmail.com>
2017-09-21 13:15:59 -0400

    Creación de vista parcial _MenuVertical y aplicarlo a todas las vistas.

--- TallerDeMotos/App_Start/BundleConfig.cs
+++ TallerDeMotos/App_Start/BundleConfig.cs
@@ -8,8 +8,11 @@ namespace TallerDeMotos
         // Para obtener más información sobre Bundles, visite http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/menu-responsive.js",
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/respond.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -18,10 +21,6 @@ namespace TallerDeMotos
             // preparado para la producción y podrá utilizar la herramienta de compilación disponible en http://modernizr.com para seleccionar solo las pruebas que necesite.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-cerulean.css",
---
+++ TallerDeMotos/Scripts/menu-responsive.js
@@ -0,0 +1,11 @@
+﻿$(".btn-menu").click(function () {
+    $(".menu-principal .menu").slideToggle();
+});
+
+$(window).resize(function () {
+    if ($(document).width() > 991) {
+        $(".menu-principal .menu").css({ "display": "block" });
+    } else {
+        $(".menu-principal .menu").css({ "display": "none" });
+    }
+});

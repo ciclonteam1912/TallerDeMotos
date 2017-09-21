d94cd258b13b714f2a81d5b46a561eec2a1bac13
Juan Moris <moris_juan@hotmail.com>
2017-09-21 12:42:26 -0400

    Creación de vistas principales para cada módulo (Taller, Ventas, Compras, Stock, y Seguridad)

---
+++ TallerDeMotos/Controllers/InicioController.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Mvc;
+
+namespace TallerDeMotos.Controllers
+{
+    public class InicioController : Controller
+    {
+        // GET: Inicio
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Taller()
+        {
+            return View();
+        }
+
+        public ActionResult Ventas()
+        {
+            return View();
+        }
+
+        public ActionResult Compras()
+        {
+            return View();
+        }
+
+        public ActionResult Stock()
+        {
+            return View();
+        }
+
+        public ActionResult Seguridad()
+        {
+            return View();
+        }
+    }
+}

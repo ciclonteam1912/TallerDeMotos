38ee7377ae13d547eb529107f36a5319212661c9
Juan Moris <moris_juan@hotmail.com>
2017-09-21 17:02:20 -0400

    Creación de Formulario Cliente.

---
+++ TallerDeMotos/Controllers/ClienteController.cs
@@ -0,0 +1,92 @@
+﻿using AutoMapper;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+using TallerDeMotos.Models;
+using TallerDeMotos.Models.ModelosDeDominio;
+using TallerDeMotos.ViewModels;
+
+namespace TallerDeMotos.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public ClienteController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            return View("ListaDeClientes");
+        }
+
+        public ActionResult NuevoCliente()
+        {
+            var tiposDocumentos = _context.TipoDocumentos.ToList();
+            var personerias = _context.Personerias.ToList();
+
+            var viewModel = new ClienteViewModel()
+            {
+                TiposDocumentos = tiposDocumentos,
+                Personerias = personerias
+            };
+
+            return View("ClienteFormulario", viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult GuardarCliente(Cliente cliente)
+        {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new ClienteViewModel(cliente)
+                {
+                    Personerias = _context.Personerias.ToList(),
+                    TiposDocumentos = _context.TipoDocumentos.ToList()
+                };
+
+                return View("ClienteFormulario", viewModel);
+            }
+
+            if (cliente.Id == 0)
+            {
+                cliente.FechaDeIngreso = DateTime.Now;
+                _context.Clientes.Add(cliente);
+            }
+            else
+            {
+                var clientesBD = _context.Clientes.Single(c => c.Id == cliente.Id);
+                Mapper.Map<Cliente, Cliente>(cliente, clientesBD);
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult EditarCliente(int id)
+        {
+            var cliente = _context.Clientes.SingleOrDefault(c => c.Id == id);
+
+            if (cliente == null)
+                return HttpNotFound();
+
+            var viewModel = new ClienteViewModel(cliente)
+            {
+                Personerias = _context.Personerias.ToList(),
+                TiposDocumentos = _context.TipoDocumentos.ToList()
+            };
+
+            return View("ClienteFormulario", viewModel);
+        }
+    }
+}
---
+++ TallerDeMotos/ViewModels/ClienteViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using TallerDeMotos.Models.ModelosDeDominio;
+
+namespace TallerDeMotos.ViewModels
+{
+    public class ClienteViewModel
+    {
+        public IEnumerable<TipoDocumento> TiposDocumentos { get; set; }
+
+        public IEnumerable<Personeria> Personerias { get; set; }
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        [Display(Name = "Teléfono")]
+        public string Telefono { get; set; }
+
+        [Display(Name = "Dirección")]
+        public string Direccion { get; set; }
+
+        [Display(Name = "Correo Electrónico")]
+        public string CorreoElectronico { get; set; }
+
+        [Display(Name = "Propietario")]
+        public string NombrePropietario { get; set; }
+
+        [Display(Name = "Fecha de Nacimiento")]
+        public DateTime? FechaDeNacimiento { get; set; }
+
+        [Display(Name = "Personería")]
+        public byte PersoneriaId { get; set; }
+
+        [Display(Name = "Tipo de Documento")]
+        public byte TipoDocumentoId { get; set; }
+
+        [Required]
+        [Display(Name = "Valor del Documento")]
+        public string ValorDocumento { get; set; }
+
+        public string Titulo
+        {
+            get
+            {
+                return Id != 0 ? "Editar Cliente" : "Nuevo Cliente";
+            }
+        }
+
+        public ClienteViewModel()
+        {
+
+        }
+
+        public ClienteViewModel(Cliente cliente)
+        {
+            Id = cliente.Id;
+            Nombre = cliente.Nombre;
+            Apellido = cliente.Apellido;
+            Telefono = cliente.Telefono;
+            Direccion = cliente.Direccion;
+            CorreoElectronico = cliente.CorreoElectronico;
+            NombrePropietario = cliente.NombrePropietario;
+            FechaDeNacimiento = cliente.FechaDeNacimiento;
+            PersoneriaId = cliente.PersoneriaId;
+            TipoDocumentoId = cliente.TipoDocumentoId;
+            ValorDocumento = cliente.ValorDocumento;
+        }
+    }
+}

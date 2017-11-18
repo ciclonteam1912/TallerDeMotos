98f56fbdbeaf52924b4cd68ec74d4211de5b6d39
Juan Moris <moris_juan@hotmail.com>
2017-11-18 16:28:25 -0300

    Agregar Cascading DropDownList de Kendo para Tipo de Producto y Producto en el detalle del Presupuesto.

---
+++ TallerDeMotos/Controllers/APIs/TipoProductosController.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Web.Http;
+using TallerDeMotos.Models;
+
+namespace TallerDeMotos.Controllers.APIs
+{
+    public class TipoProductosController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public TipoProductosController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        [HttpGet]
+        public IHttpActionResult ObtenerTiposDeProductos()
+        {
+            var tipoProductos = _context.ProductoTipos.ToList();
+
+            return Ok(tipoProductos);
+        }
+    }
+}
